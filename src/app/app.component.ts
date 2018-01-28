6db2a36df6bfed5c0350fb728e91ecd614c090e1
boputu <borjabussiness@gmail.com>
2018-01-28 21:14:12 +0100

    Proyecto angular CV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
+  selector: 'primeraComponente'
+
 })
+
 export class AppComponent {
   title = 'app';
+  nombre:string;
+  apellidos:string;
+  fecha:number;
+  puesto:string;
+  colores:number;
+  carnet:number;
+
+
+  fondo(event){
+    this.colores = event.target.value;
+  }
+
+  conducir(event){
+    this.carnet = event.target.value;
+  }
+
+  inputs(event){
+    var id = event.target.id;
+    if (id == 'nombre'){
+      this.nombre = event.target.value;
+    }else if(id == 'apellidos'){
+      this.apellidos = event.target.value;
+    }else if(id == 'fecha'){
+      this.fecha = event.target.value;
+    }else if(id == 'puesto'){
+      this.puesto = event.target.value;
+    }
+    }
+
+    }
+    }
+  }
+
 }
